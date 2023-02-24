5b7d8548bd12e0710cc5975cba5f4a6fdd9543a8
fgc <fenggc@zhejianglab.com>
2023-02-24 22:28:25 +0800

    bfs by template

--- graph/graph_undered_set_demo.cpp
+++ graph/graph_undered_set_demo.cpp
@@ -1,13 +1,14 @@
 /*
  * @Author: fenggaochao
  * @Date: 2023-02-24 12:59:45
- * @LastEditTime: 2023-02-24 13:26:41
+ * @LastEditTime: 2023-02-24 18:32:17
  * @LastEditors: fenggaochao
  * @Description: 
  * @FilePath: /C++_Mini_Program/graph/graph_undered_set_demo.cpp
  */
 #include "graph_struct_set.h"
 #include "depth_first_path_template.h"
+#include "breadth_first_paths_template.h"
 #include "common_util.h"
 
 int main(int argc,char* argv[])
@@ -23,25 +24,10 @@ int main(int argc,char* argv[])
   DepthFirstPaths<GraphByUnorderSet> dfs_path(graph_1, s);
   std::cout << "0-1: " << dfs_path.HasPathTo(1);
   std::cout << " dfs paths: " << std::endl;
-  // for (int v = 0; v < graph_1.Vertex(); ++v)
-  // {
-  //   if (dfs_path.HasPathTo(v))
-  //   {
-  //     std::cout << s << " to " << v << ": ";
-  //     for (auto x : dfs_path.PathTo(v))
-  //     {
-  //       if (x == s)
-  //       {
-  //         std::cout << x;
-  //       }
-  //       else
-  //       {
-  //         std::cout << "-" << x;
-  //       }
-  //     }
-  //     std::cout << std::endl;
-  //   }
-  // }
   PrintGraphPath(graph_1, dfs_path, s);
+
+  BreadthFirstPaths<GraphByUnorderSet> bfs_path(graph_1, s);
+  std::cout << " bfs paths: " << std::endl;
+  PrintGraphPath(graph_1, bfs_path, s);
   return 0;
 }
