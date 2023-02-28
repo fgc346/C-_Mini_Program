023cf343134b9e5a412a4d8d0cfcb61355606235
fgc <fenggc@zhejianglab.com>
2023-02-28 09:37:49 +0800

    add prority_queue using demo

--- stl_using/unodered_set_using.cpp
+++ stl_using/unodered_set_using.cpp
@@ -25,7 +25,8 @@ int main()
         std::cout << std::endl;
     }
     // 这样使用是有问题的，暂时没有发现这个样用的问题在哪里
-    //GetSet() 函数返回一个对象
+    // GetSet() 函数返回一个临时对象，执行完下面的一条语句后
+    // iter_1迭代器已经执行一个消亡的对象，就出现问题了。
     auto iter_1 = GetSet(0).begin();
     while (iter_1 != GetSet(0).end())
     {
@@ -33,6 +34,23 @@ int main()
         ++iter_1;
     }
     std::cout << std::endl;
+
+    auto iter = GetSet2(0).find(10);
+    if (iter != GetSet2(0).end())
+    {
+      std::cout << *iter << std::endl;
+    }
+    // std::cout << *iter << std::endl;
+    //GetSet() 函数返回一个引用
+    auto uset_1 = GetSet2(0);
+    auto iter_12 =uset_1.begin();
+    while (iter_12 != uset_1.end())
+    {
+        std::cout << *iter_12 << " ";
+        ++iter_12;
+    }
+    std::cout << std::endl;
+
     //GetSet() 函数返回一个引用
     auto iter_2 = GetSet2(0).begin();
     while (iter_2 != GetSet2(0).end())
